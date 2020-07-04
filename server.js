11cc98360b03bd7134591b7b5d9a4c7e022c2de1
Lalitus <tallitosan@gmail.com>
2020-07-04 00:04:26 -0500

    reary to deploy in heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,13 +10,27 @@ connectDB();
 // Init middleware
 app.use(express.json({extended: false}));
 
-app.get('/', (req,res, next) => res.json({msg: 'Welcome to contact keeper API'}));
+// this needs to be commented for production
+// app.get('/', (req,res, next) => res.json({msg: 'Welcome to contact keeper API'}));
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+ 
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    // set static folder 
+    app.use(express.static('client/build'));
 
+    app.get('*', (req, res, next) => res.sendFile(
+        path.resolve(
+            __dirname,
+            'client',
+            'build',
+            'index.html'
+        )));
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()  => console.log(`Server started on port: ${PORT}`));
