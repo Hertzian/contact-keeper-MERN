7222541fb82b450729b03f13554161315820d157
Lalitus <tallitosan@gmail.com>
2020-07-04 05:14:10 -0500

    minor typo fixes

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -19,6 +19,7 @@ export const ContactItem = ({contact}) => {
 
     return (
         <div className='card bg-light'>
+
             <h3 className="text-primary text-left">
                 {name}{' '} 
                 <span
@@ -28,6 +29,7 @@ export const ContactItem = ({contact}) => {
                     {type.charAt(0).toUpperCase() + type.slice(1)}
                 </span>
             </h3>
+
             <ul className="list">
                 {email && (
                     <li>
@@ -40,10 +42,12 @@ export const ContactItem = ({contact}) => {
                     </li>
                 )}
             </ul>
+
             <p>
                 <button className="btn btn-dark btn-sm" onClick={() => setCurrent(contact)}>Edit</button>
                 <button className="btn btn-danger btn-sm" onClick={onDelete}>Delete</button>
             </p>
+
         </div>
     );
 }
